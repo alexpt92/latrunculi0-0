9dab73e527d15b8a7e982a33f6cebe70ba3dc9f3
alpet <s4alpett@uni-trier.de>
2020-08-03 20:10:41 +0200

    new dynamic version added

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,173 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using System;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
 
-    public boardScript board;
-
-    public int x { get; set; }//Brettgröße X & Y
-    public int y { get; set; }
-    public float xCoord;
-    public float yCoord;
     public int sizeX;
     public int sizeY;
-    public pathFinder pathFinder;
-    public Sprite o_Sprite;
-    string lastSelectedButtonName = null;
+    public GameObject wPrefab;
+    public GameObject bPrefab;
 
-    public Sprite m_Sprite;
+    public Board mBoard;
+    public PieceManager mPieceManager;
 
     private bool gameRunning = false;
-   // private int maxRounds = 10;
-    private int currentRound = 1;
-    private int maxPlayers = 2;
-    private int currentPlayer = 1;
-    private bool eliminatedInPrevTurn = false;
-    string winner = null;
-    //private SoundManagerScript soundManager;
-   // private int maxBalls = 2;
-  // private int currentBall = 1;
+    // private int maxRounds = 10;
+  //  private int currentRound = 1;
+    //private int maxPlayers = 2;
+    //private int currentPlayer = 1;
+   // string winner = null;
 
+
+    private bool moveAgain;
+
+    // Start is called before the first frame update
     void Start()
     {
-        board = new boardScript(sizeX, sizeY, xCoord, yCoord, m_Sprite, o_Sprite);
-        pathFinder = new pathFinder();
-        gameRunning = true;
-        ParticleSystem particleSystemWin = GameObject.Find("ParticlesWin").GetComponent<ParticleSystem>();
-       // soundManager = new SoundManagerScript();
-        //particleSystemWin.Play();
-        particleSystemWin.Stop();
+        //Create Board
+        mBoard.Create();
+
+
+        //Create Pieces
+
+        mPieceManager.Setup(mBoard);
+        Camera.main.transform.position = new Vector3(1, 1, 1);
+        Camera.main.orthographicSize = 9 * 100;
+        Camera.main.gameObject.SetActive(true);
+
+
+        GameObject.FindGameObjectWithTag("BoardCanvas").transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 125));
+        GameObject.FindGameObjectWithTag("Pieces").transform.position = GameObject.FindGameObjectWithTag("BoardCanvas").transform.position;
+
+        //Camera.main.orthographicSize = 9 * 40;
+
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (gameRunning)
-        {
-            if (currentPlayer == 1)
-            {
-                Button[] bButtons = board.bList.ToArray();
-                for (int i = 0; i < bButtons.Length; i++)
-                {
-                    bButtons[i].interactable = false;
-                }
-                Button[] wButtons = board.wList.ToArray();
-                for (int i = 0; i < wButtons.Length; i++)
-                {
-                    wButtons[i].interactable = true;
 
-                }
-            }
-
-            if (currentPlayer == 2)
-            {
-                Button[] bButtons = board.bList.ToArray();
-                for (int i = 0; i < bButtons.Length; i++)
-                {
-                    bButtons[i].interactable = true;
-                }
-                Button[] wButtons = board.wList.ToArray();
-                for (int i = 0; i < wButtons.Length; i++)
-                {
-                    wButtons[i].interactable = false;
-
-                }
-            }
-        }
-    }
-
-    public void clicked()
-    {
-        if (gameRunning)
-        {
-            string selectedName = EventSystem.current.currentSelectedGameObject.name;
-            if (selectedName.Contains("Button"))
-            {
-                Button actButton = GameObject.Find(selectedName).GetComponent<Button>();
-                board.LocateField(selectedName);
-                lastSelectedButtonName = selectedName;
-                // board.HighlightFields();
-            }
-
-            else if (lastSelectedButtonName != null)
-            {
-                //String[] seperator = { "x" };
-                //String[] fieldPos = selectedName.Split(seperator);
-                Image clickedField = GameObject.Find(selectedName).GetComponent<Image>();
-                board.moveButton(lastSelectedButtonName, clickedField);
-                //soundManager.
-                //soundManager.
-                SoundManagerScript.PlaySound("move");
-                board.checkAttack(lastSelectedButtonName);
-                lastSelectedButtonName = null;
-                eliminatedInPrevTurn = board.moveAgain;
-                checkForWin();
-
-                if (winner != null)
-                {
-                    if (winner == "white")
-                    {
-                        Debug.Log("Player1 wins");
-                        ParticleSystem particleSystemWin = GameObject.Find("ParticlesWin").GetComponent<ParticleSystem>();
-                        particleSystemWin.Play();
-                    }
-                    else if (winner == "black")
-                    {
-                        Debug.Log("Player2 wins");
-                        ParticleSystem particleSystemWin = GameObject.Find("ParticlesWin").GetComponent<ParticleSystem>();
-                        particleSystemWin.Play();
-
-                    }
-                }
-                NextPlayer();
-            }
-        }
-    }
-   
-
-    void checkForWin()
-    {
-        Button[] bArray = board.bList.ToArray();
-        Button[] wArray = board.wList.ToArray();
-
-        if (bArray.Length < 2)
-        {
-            winner = "white";
-            board.disablePieces();
-            gameRunning = false;
-
-        }
-        else if (wArray.Length < 2)
-        {
-            winner = "black";
-            board.disablePieces();
-            gameRunning = false;
-
-        }
-    }
-    void NextPlayer()
-    {
-        if (eliminatedInPrevTurn) return;
-        if (currentPlayer == 1)
-        {
-            currentPlayer = 2;
-        }
-        else
-        {
-            //NextRound();
-            currentPlayer = 1;
-        }
-    }
-
-    void NextRound()
-    {
-        currentRound++;
-    }
 }
--- Assets/Piece.cs
+++ Assets/Piece.cs
@@ -1,24 +1,315 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Piece : MonoBehaviour
+public class Piece : EventTrigger
 {
 
-    public string name { get; set; }
-    public float x { get; set; }
-    public float y { get; set; }
-    // Start is called before the first frame update
-    public Piece(string newName, float newX, float newY) 
+    [HideInInspector]
+    public Color mColor = Color.clear;
+    public Sprite n_Sprite;
+    public Sprite o_Sprite;
+
+    private int sizeX;
+    private int sizeY;
+
+    protected Cell mOriginalCell = null;
+    protected Cell mCurrentCell = null;
+
+    protected RectTransform mRectTransform = null;
+    protected PieceManager mPieceManager;
+
+    protected Cell mTargetCell = null;
+
+    protected Vector3Int mMovement;// = new Vector3Int(sizeX, sizeY, 1);
+    protected List<Cell> mHighlightedCells = new List<Cell>();
+
+
+    public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager, Vector3Int movement)
     {
-        name = newName;
-        x = newX;
-        y = newY;
+        mPieceManager = newPieceManager;
+
+        mColor = newTeamColor;
+        GetComponent<Image>().color = newSpriteColor;
+        mRectTransform = GetComponent<RectTransform>();
+        mMovement = movement;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Place(Cell newCell)
     {
-        
+        //Cell
+        mCurrentCell = newCell;
+        mOriginalCell = newCell;
+        mCurrentCell.mCurrentPiece = this;
+
+        //Object
+        transform.localPosition = mCurrentCell.transform.localPosition;
+
+        gameObject.SetActive(true);
     }
+
+    public void Reset()
+    {
+        Kill();
+
+        Place(mOriginalCell);
+
+    }
+
+    public virtual void Kill()
+    {
+        //Clear Cell
+        mCurrentCell.mCurrentPiece = null;
+
+        //Remove Piece
+        gameObject.SetActive(false);
+
+    }
+
+    #region movement
+
+    protected virtual void Move()
+    {
+        // if enemy piece -> back to currentCell
+        if (mTargetCell.mCurrentPiece != null)
+        {
+            transform.position = mCurrentCell.transform.position;
+            mTargetCell = null;
+
+            return;
+        }
+
+        Color color = mColor;
+
+        //check neighbors for Enemys & Allys
+        Cell[] attackedCells = checkNeighbor();
+        if (attackedCells != null) {
+            for (int i = 0; i <= attackedCells.Length - 1; i++)
+            {
+                if (attackedCells[i] != null)
+                {
+                    attackedCells[i].RemovePiece();
+                }
+            }
+        }
+        if (attackedCells != null)
+        {
+            if (color == Color.white)
+                color = Color.black;
+            else
+                color = Color.white;
+        }
+
+        mCurrentCell.mCurrentPiece = null;
+
+        mCurrentCell = mTargetCell;
+        mCurrentCell.mCurrentPiece = this;
+
+        transform.position = mCurrentCell.transform.position;
+        mTargetCell = null;
+        mPieceManager.SwitchSides(color);
+
+    }
+
+    private Cell[] checkNeighbor()
+    {
+        int currentX = mTargetCell.mBoardPosition.x;
+        int currentY = mTargetCell.mBoardPosition.y;
+
+        int targetX = mTargetCell.mBoardPosition.x + 1; 
+        int targetY = mTargetCell.mBoardPosition.y;
+
+        int allyX = mTargetCell.mBoardPosition.x + 2;
+        int allyY = mTargetCell.mBoardPosition.y;
+
+        List<Cell> attackedCells = new List<Cell>();
+
+        int counter = 0;
+
+        if (mCurrentCell.mBoard.ValidateCell(targetX, targetY, this) == CellState.Enemy
+            && mCurrentCell.mBoard.ValidateCell(allyX, allyY, this) == CellState.Friendly)
+        {
+            counter++;
+            attackedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+        }
+
+        targetX = mTargetCell.mBoardPosition.x - 1;
+        targetY = mTargetCell.mBoardPosition.y;
+
+        allyX = mTargetCell.mBoardPosition.x - 2;
+        allyY = mTargetCell.mBoardPosition.y;
+
+        if (mCurrentCell.mBoard.ValidateCell(targetX, targetY, this) == CellState.Enemy
+    && mCurrentCell.mBoard.ValidateCell(allyX, allyY, this) == CellState.Friendly)
+        {
+            counter++;
+            attackedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+        }
+
+        targetX = mTargetCell.mBoardPosition.x;
+        targetY = mTargetCell.mBoardPosition.y + 1;
+
+        allyX = mTargetCell.mBoardPosition.x;
+        allyY = mTargetCell.mBoardPosition.y + 2;
+
+        if (mCurrentCell.mBoard.ValidateCell(targetX, targetY, this) == CellState.Enemy
+    && mCurrentCell.mBoard.ValidateCell(allyX, allyY, this) == CellState.Friendly)
+        {
+            counter++;
+            attackedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+        }
+
+        targetX = mTargetCell.mBoardPosition.x;
+        targetY = mTargetCell.mBoardPosition.y - 1;
+
+        allyX = mTargetCell.mBoardPosition.x;
+        allyY = mTargetCell.mBoardPosition.y - 2;
+
+        if (mCurrentCell.mBoard.ValidateCell(targetX, targetY, this) == CellState.Enemy
+    && mCurrentCell.mBoard.ValidateCell(allyX, allyY, this) == CellState.Friendly)
+        {
+            counter++;
+            attackedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+        }
+
+        if (attackedCells.ToArray().Length > 0)
+            return attackedCells.ToArray();
+        else
+            return null;
+    }
+
+    public void CreateCellPath(int xDirection, int yDirection, int movement)
+    {
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+
+        for (int i = 1; i<= movement; i++)
+        {
+            currentX += xDirection;
+            currentY += yDirection;
+
+            CellState cellState = CellState.None;
+            cellState = mCurrentCell.mBoard.ValidateCell(currentX, currentY, this);
+
+            //If enemy, break;
+            if (cellState == CellState.Enemy)
+            {
+                break;
+            }
+
+            //get CellState
+            if (cellState != CellState.Free)
+                break;
+
+            //ADD to Highlighted List
+                mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[currentX, currentY]);
+
+
+
+
+        }
+
+
+
+    }
+
+    protected virtual void CheckPathing()
+    {
+        //Horizontal
+        CreateCellPath(1, 0, mMovement.x);
+        CreateCellPath(-1, 0, mMovement.x);
+
+        //Vertical
+        CreateCellPath(0, 1, mMovement.y);
+        CreateCellPath(0, -1, mMovement.y);
+
+      /*  //Upper diagonal
+         CreateCellPath(1, 1, mMovement.z);
+         CreateCellPath(-1, 1, mMovement.z);
+
+         //Lower diagonal
+         CreateCellPath(-1, -1, mMovement.z);
+         CreateCellPath(1, -1, mMovement.z);*/
+    }
+
+
+    protected void ShowCells()
+    {
+        foreach (Cell cell in mHighlightedCells)
+            cell.highlight();
+
+    }
+
+    protected void ClearCells()
+    {
+        foreach (Cell cell in mHighlightedCells)
+            //cell.mOutlineImage.enabled = false;
+            cell.removeHighlight();
+
+        mHighlightedCells.Clear();
+    }
+    #endregion
+
+
+    #region Events
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+
+        CheckPathing();
+
+        ShowCells();
+    }
+
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+
+        //follow cursor Position
+        transform.position += (Vector3)eventData.delta;
+
+
+        foreach (Cell cell in mHighlightedCells)
+        {
+            if (RectTransformUtility.RectangleContainsScreenPoint(cell.mRectTransform, Input.mousePosition))
+            {
+                // if mouse is in a valid cell -> get it
+                mTargetCell = cell;
+                break;
+            }
+            //if mouse is not in highlighted cell -> no valid move
+            mTargetCell = null;
+        }
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+        ClearCells();
+
+        if (!mTargetCell)
+        {
+            transform.position = mCurrentCell.gameObject.transform.position;
+            return;
+        }
+
+        Move();
+
+    }
+    #endregion
+
+    public bool isDead ()
+    {
+        if (mCurrentCell.mCurrentPiece == null)
+            return true;
+        else
+
+            return false;
+    }
+
 }
